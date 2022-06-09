a7825fc7f12c978e092245beee83040f8e00ab45
Yash Dani <yashdani18@gmail.com>
2022-06-09 00:17:46 -0700

    screenshot & final commit

--- minds.py
+++ minds.py
@@ -2,23 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from tqdm import tqdm
 from time import sleep
 
-sia = SentimentIntensityAnalyzer()
-
-response = requests.get('https://www.aljazeera.com/where/mozambique/').text
-
-soup = BeautifulSoup(response, 'lxml')
-
-contents = soup.find_all(class_='gc__content')
-
-stopwords = nltk.corpus.stopwords.words('english')
-
+# global variables initialization
 list_titles = []
 list_exc = []
 
@@ -27,76 +17,84 @@ list_pos = []
 list_neg = []
 list_neu = []
 
-for index, content in enumerate(contents):
-    title = str(content.find('a').find('span').text)
-    title = title.replace('-', '')
-    excerpt = str(content.find(class_="gc__excerpt").find('p').text)
-    excerpt = excerpt.replace('-', '')
+# initialize nltk
+sia = SentimentIntensityAnalyzer()
 
-    print(title)
-    print(excerpt)
+# web scraping
+response = requests.get('https://www.aljazeera.com/where/mozambique/').text
+soup = BeautifulSoup(response, 'lxml')
+contents = soup.find_all(class_='gc__content')
+
+# iterating through news articles
+for index, content in enumerate(contents):
+    # stop iterating when 10 news articles collected
+    # if index == 10:
+    #     break
+
+    # extract news headline
+    title = str(content.find('a').find('span').text)
+
+    # extract news excerpt
+    excerpt = str(content.find(class_="gc__excerpt").find('p').text)
+
+    print('Title: ' + title)
+    print('Excerpt: ' + excerpt.replace('U+002D', ''))
+    print()
+
+    # create corresponding lists for the news title and excerpts
+    list_titles.append(title)
+    list_exc.append(excerpt)
+
+    # generate sentiment analysis for title and excerpt
     title_sia = sia.polarity_scores(title)
     exc_sia = sia.polarity_scores(excerpt)
 
-    for i in tqdm(range(0, 100), desc="Text You Want"):
-        sleep(.1)
-
+    # prepare lists of relevant data to be passed to dataframe
+    # corresponding df will be passed to plotly for polarity visualization
     list_excerpt.append('news: ' + str(index))
     list_pos.append(exc_sia['pos'])
     list_neg.append(exc_sia['neg'])
     list_neu.append(exc_sia['neu'])
 
-    print(title_sia)
-    print(exc_sia)
-
-    list_titles.append(title)
-    list_exc.append(excerpt)
     print()
 
 
+# create df for plotly
 df = pd.DataFrame(list(zip(list_exc, list_pos, list_neg, list_neu)))
 df.columns = ['news', 'pos', 'neg', 'neu']
 
-# Create figure with secondary y-axis
+# Plotly - Create figure with secondary y-axis
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-# Add traces
+# Add subplots for positive polarity news
 fig.add_trace(
-    go.Scatter(x=list_excerpt, y=list_pos, name="yaxis data"),
+    go.Scatter(x=list_excerpt, y=list_pos, name="Polarity > 0"),
     secondary_y=False,
 )
 
+# add subplots for negative polarity news
 fig.add_trace(
-    go.Scatter(x=list_excerpt, y=list_neg, name="yaxis2 data"),
+    go.Scatter(x=list_excerpt, y=list_neg, name="Polarity < 0"),
     secondary_y=True,
 )
 
+# add subplots for neutral polarity news
 fig.add_trace(
-    go.Scatter(x=list_excerpt, y=list_neu, name="yaxis2 data"),
+    go.Scatter(x=list_excerpt, y=list_neu, name="Polarity = 0"),
     secondary_y=True,
 )
 
-# Add figure title
+# add figure
 fig.update_layout(
-    title_text="Double Y Axis Example"
+    title_text="Sentiment Analysis plot"
 )
 
 # Set x-axis title
-fig.update_xaxes(title_text="xaxis title")
+fig.update_xaxes(title_text="news")
 
 # Set y-axes titles
-fig.update_yaxes(title_text="<b>primary</b> yaxis title", secondary_y=False)
-fig.update_yaxes(title_text="<b>secondary</b> yaxis title", secondary_y=True)
+fig.update_yaxes(title_text="pos yaxis title", secondary_y=False)
+fig.update_yaxes(title_text="neg yaxis title", secondary_y=True)
+fig.update_yaxes(title_text="neu yaxis title", secondary_y=True)
 
 fig.show()
-
-words_title = [word for word in list_titles if word.isalpha() and word not in stopwords]
-words_excerpt = [word for word in list_exc if word.isalpha() and word not in stopwords]
-
-print(words_title)
-print()
-print(words_excerpt)
-
-
-print()
-
